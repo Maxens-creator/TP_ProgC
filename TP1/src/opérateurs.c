93ae82d4b11086c58657b1679155c351563c577d
Maxens-creator <150241086+Maxens-creator@users.noreply.github.com>
2026-09-18 09:02:58 +0000

    EX1.5

--- TP1/src/opérateurs.c
+++ TP1/src/opérateurs.c
@@ -1,2 +1,23 @@
+#include <stdio.h>
+#include <stdbool.h>
 
+int main(void) {
+    int a = 16;
+    int b = 3;
 
+    /* Opérateurs arithmétiques */
+    printf("Addition (a + b) : %d\n", a + b);
+    printf("Soustraction (a - b) : %d\n", a - b);
+    printf("Multiplication (a * b) : %d\n", a * b);
+    printf("Division entière (a / b) : %d\n", a / b);
+    printf("Modulo (a %% b) : %d\n\n", a % b);
+
+    /* Opérateurs de comparaison / logiques */
+    bool est_egal = (a == b);
+    bool est_superieur = (a > b);
+
+    printf("a == b : %s (%d)\n", est_egal ? "true" : "false", est_egal);
+    printf("a > b  : %s (%d)\n", est_superieur ? "true" : "false", est_superieur);
+
+    return 0;
+}
