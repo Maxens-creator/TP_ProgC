5766db3acbd1a640b0614bddbb14b0bb8d8d0dc6
Maxens-creator <150241086+Maxens-creator@users.noreply.github.com>
2026-09-18 09:06:14 +0000

    EX1.6 b

--- TP1/src/boucles.c
+++ TP1/src/boucles.c
@@ -8,16 +8,19 @@ int main(void) {
         return 1;
     }
 
-    for (int i = 1; i <= compteur; i++) {
-        for (int j = 1; j <= i; j++) {
-            // Première colonne, dernière colonne de la ligne ou dernière ligne complète
+    int i = 1;
+    while (i <= compteur) {
+        int j = 1;
+        while (j <= i) {
             if (j == 1 || j == i || i == compteur) {
                 printf("* ");
             } else {
                 printf("# ");
             }
+            j++;
         }
         printf("\n");
+        i++;
     }
 
     return 0;
