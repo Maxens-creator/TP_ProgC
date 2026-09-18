e37ffcb94f493a08b9e04011245684c8a73fd7bc
Maxens-creator <150241086+Maxens-creator@users.noreply.github.com>
2026-09-18 09:18:57 +0000

    EX2.1

---
+++ TP2/src/puissance.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void) {
+    int a = 2;
+    int b = 3;
+    long long resultat = 1;
+
+    // Calcul de a^b pour b >= 0
+    for (int i = 0; i < b; i++) {
+        resultat *= a;
+    }
+
+    printf("%d élevé à la puissance %d = %lld\n", a, b, resultat);
+
+    return 0;
+}
